d15243fc3424db233cbb99d3afb46629661b196d
NOLWE <andreyrmirnovhtc@gmail.com>
2015-03-15 18:26:28 +0400

    new thread and add some code in main

---
+++ app/src/main/java/com/testgit/nolwe/testingthread/RunnableThread2.java
@@ -0,0 +1,11 @@
+package com.testgit.nolwe.testingthread;
+
+/**
+ * Created by Nolwe on 15.03.2015.
+ */
+public class RunnableThread2 implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
